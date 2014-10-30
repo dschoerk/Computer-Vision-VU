ee233071b655b88063618e529e56cc74b13701a5
Oana Moraru <tsunade_oana@yahoo.com>
2014-10-30 11:35:29 +0100

    removed some comments

--- Ex1/As1 - Colorizing Images/colorize.m
+++ Ex1/As1 - Colorizing Images/colorize.m
@@ -4,56 +4,6 @@
 path = '../resources'; 
 images_nr = 6;
 
-%R_names = cell(images_nr, 1);
-%G_names = cell(images_nr, 1);
-%B_names = cell(images_nr, 1);
-
-%r_size = 1; %size of R array
-%g_size = 1; %size of G array
-%b_size = 1; %size of B array
-
-%loading the files from path into images array
-%files = loadImages(path);
-
-%separate images from returned images array into R, G, B array
-
-%for i = 3:24  %not sure why
-    
-    %add image file containing _R to R_names array
-%    r = strfind(files(i).name, '_R');
-%    if (r ~= 0)
-%        R_names{r_size} = files(i).name;
-%        r_size = r_size + 1;
-%    end
-    
-    %add image file containing _G to G_names array
-%    g = strfind(files(i).name, '_G');
-%    if (g ~= 0)
-%        G_names{g_size} = files(i).name;
-%        g_size = g_size + 1;
-%    end
-    
-    %add image file containing _B to B_names array
-%    b = strfind(files(i).name, '_B');
-%    if (b ~= 0)
-%        B_names{b_size} = files(i).name;
-%        b_size = b_size + 1;
-%    end
-
-%end
-
-%finally read in images (in separate arrays)
-%images_R = cell(images_nr, 1);
-%images_G = cell(images_nr, 1);
-%images_B = cell(images_nr, 1);
-
-%for i = 1:images_nr
-%    images_R{i} = imread(strcat(path, '/', R_names{i}));
-%    images_G{i} = imread(strcat(path, '/', G_names{i}));
-%    images_B{i} = imread(strcat(path, '/', B_names{i}));
-%end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %images_R = cell(images_nr, 1);
 %images_G = cell(images_nr, 1);
 %images_B = cell(images_nr, 1);
