be1c4f9d89c379c1b46ad9d6887cb358bdc150ad
unknown <a.sheldon@gmx.at>
2014-11-01 17:30:26 +0100

    Wenn ein Cluster keine Elemente hat, wird nun der entfernteste Punkt aller Cluster als neuer Centroid gewählt. Sieht richtig aus muss das aber morgen oder so nochmal checken.

--- Ex1/As2 - k-meanclustering/Assignment2.m
+++ Ex1/As2 - k-meanclustering/Assignment2.m
@@ -1,5 +1,5 @@
 path = 'E:\TU_Andi/Computervision/simple.PNG';
-K = 2; % number of clusters
+K = 3; % number of clusters
 D = 3; % 3 or 5
 r = []; %mapping of the points to the clusters
 J_old = inf; %initial value of J_old, have been bigger then the start value of J
@@ -15,7 +15,7 @@ dim = size(img);
 %random Clusterstartingpoints
 my_kx = randperm(dim(1),K);
 my_ky = randperm(dim(2),K);
-my_k = [];
+my_k = zeros(K,D);
 
 if D == 3
     for i=1:1:K
@@ -31,7 +31,11 @@ else
     end
 end
 
+%wird ein cluster 0 nimmt man aus einem Cluster den Punkt, der am
+%weitesten vom Centroid entfernt ist als neuen Clustercentroid
+
 while abs(J_old - J) > epsilon 
+
             test = J_old - J
     J_old = J;
 
@@ -40,6 +44,7 @@ while abs(J_old - J) > epsilon
     r = zeros(dim(1)*dim(2),K); %mapping matrix
     sum_k = zeros(K,D); %matrix for the summ of the distances for each cluster
     norm = zeros(1,K);  %vektor for the normalization of the computation for the new my_k  
+    dist = zeros(dim(1)*dim(2),K);
     
     pos = 0;
     for i = 1:1:dim(1)
@@ -60,9 +65,9 @@ while abs(J_old - J) > epsilon
             index = 0;
             for k = 1:1:K
                 %dist(pos) = (pdist([data(pos,:);my_k(k,:)],'euclidean'))^2;
-                dist(pos) = (pdist([data;my_k(k,:)],'euclidean'))^2;
-                if(dist(pos) < mindist) %searches for the minimum distance of the datapoint to the clusters
-                    mindist = dist(pos);
+                dist(pos,k) = (pdist([data;my_k(k,:)],'euclidean'))^2;
+                if(dist(pos,k) < mindist) %searches for the minimum distance of the datapoint to the clusters
+                    mindist = dist(pos,k);
                     index = k;
                 end
             end
@@ -73,8 +78,30 @@ while abs(J_old - J) > epsilon
             norm(index) = norm(index) + 1; %for the normalization
         end
     end
-    for k = 1:1:K
-        my_k(k,:) = sum_k(k,:)/norm(k); % compute the new my_k's
+    
+    J = sum(sum(dist));
+    if abs(J_old - J) <= epsilon
+        break
     end
-    J = sum(dist); 
+    
+    for k = 1:1:K
+        if norm(k) == 0 %cluster has no elements
+            [dex,e] = max(dist(:));
+            in_k = fix(e/(dim(1)*dim(2)))+1; %in what cluster is the maximum
+            exaktpos = mod(e,dim(1)*dim(2)); %position in the cluster
+            new_y = fix(exaktpos/dim(1))+1; %y-coordinate in the imige
+            new_x = mod(exaktpos ,dim(1)); %x-coordinate in the image
+            
+            my_k(k,1) = img (new_x,new_y,1);
+            my_k(k,2) = img (new_x,new_y,2);
+            my_k(k,3) = img (new_x,new_y,3);
+            if D == 5
+                my_k(k,4) = new_y/dim(1);
+                my_k(k,5) = new_x/dim(2);
+            end
+        else
+            my_k(k,:) = sum_k(k,:)/norm(k); % compute the new my_k's
+        end
+    end
+    %J = sum(sum(dist));
 end
