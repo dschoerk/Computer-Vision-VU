447d4e7f11f11b8d93f77e94d97a968184169156
Oana Moraru <tsunade_oana@yahoo.com>
2015-01-05 15:49:30 +0100

    Bag of Words finished

--- Ex2/Bag of Words/main.m
+++ Ex2/Bag of Words/main.m
@@ -1,6 +1,7 @@
-%% TODO description
-
-
+%% Description:
+%   main.m is a script that performs the three basic steps of the Bag 
+%   of visual Words classification method by calling the functions: 
+%   Build Vocabulary, BuildKNN and ClassifyImages.
 
 
 C = BuildVocabulary('train', 50);
@@ -8,3 +9,6 @@ C = BuildVocabulary('train', 50);
 [training, group] = BuildKNN('train', C);
 
 conf_matrix = ClassifyImages('test', C, training, group);
+
+% print out resulting confusion matrix
+conf_matrix
